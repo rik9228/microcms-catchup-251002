3c93bddab67dbd808ee051ec074a0cec33716a1c
rik9228 <rank228130@gmail.com>
2025-10-03 23:42:19 +0900

    add：news-detail

--- assets/js/post.js
+++ assets/js/post.js
@@ -12,37 +12,37 @@ const ENDPOINT = `https://${SERVICE_DOMAIN}.microcms.io/api/v1/news/${newsId}`;
 /**
  * ニュース詳細ページ
  */
-// async function fetchDetail() {
-//   const titleEl = document.getElementById("news-title");
-//   const dateEl = document.getElementById("news-date");
-//   const categoryEl = document.getElementById("news-category");
-//   const contentEl = document.getElementById("news-content");
-//   const thumbnailEl = document.getElementById("news-thumbnail");
+async function fetchDetail() {
+  const titleEl = document.getElementById("news-title");
+  const dateEl = document.getElementById("news-date");
+  const categoryEl = document.getElementById("news-category");
+  const contentEl = document.getElementById("news-content");
+  const thumbnailEl = document.getElementById("news-thumbnail");
 
-//   try {
-//     const res = await fetch(ENDPOINT, {
-//       headers: { "X-API-KEY": API_KEY }
-//     });
-//     const data = await res.json();
+  try {
+    const res = await fetch(ENDPOINT, {
+      headers: { "X-API-KEY": API_KEY }
+    });
+    const data = await res.json();
 
-//     titleEl.textContent = data.title;
+    titleEl.textContent = data.title;
 
-//     // 日付
-//     dateEl.textContent = `公開日: ${data.publishedAt?.slice(0,10)}`;
+    // 日付
+    dateEl.textContent = `公開日: ${data.publishedAt?.slice(0,10)}`;
 
-//     // 本文（microCMSからのcontentはHTML文字列）
-//     categoryEl.innerHTML = `カテゴリー: ${data.category.name}`;
+    // 本文（microCMSからのcontentはHTML文字列）
+    categoryEl.innerHTML = `カテゴリー: ${data.category.name}`;
     
-//     // 本文（microCMSからのcontentはHTML文字列）
-//     contentEl.innerHTML = data.content;
+    // 本文（microCMSからのcontentはHTML文字列）
+    contentEl.innerHTML = data.content;
 
-//     // サムネイル
-//     thumbnailEl.src = data.thumbnail.url;
+    // サムネイル
+    thumbnailEl.src = data.thumbnail ? data.thumbnail.url : "https://dummyimage.com/960x600.jpg";
 
-//   } catch (error) {
-//     console.error("詳細取得エラー:", error);
-//     contentEl.innerHTML = "<p>記事を読み込めませんでした。</p>";
-//   }
-// }
+  } catch (error) {
+    console.error("詳細取得エラー:", error);
+    contentEl.innerHTML = "<p>記事を読み込めませんでした。</p>";
+  }
+}
 
-// fetchDetail();
+fetchDetail();
